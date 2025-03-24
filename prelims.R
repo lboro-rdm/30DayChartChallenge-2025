0eca30a913ae69552e3d66209d3859c23be03edc
lboro-rdm <rdm@lboro.ac.uk>
2025-03-24 16:13:54 +0000

    inital commit

---
+++ prelims.R
@@ -0,0 +1,28 @@
+library(scales)
+library(readxl)
+library(tidyverse)
+
+show_col("#361163") #primary 1
+show_col("#B70062") #primary 2
+show_col("#8D9C27") #accent
+show_col("#f2f2f2") #background
+
+
+# Data cleaning -----------------------------------------------------------
+
+data_2016_2022 <- read_excel("RawData/2016-2022.xlsx")
+data_2023 <- read_excel("RawData/2023.xlsx")
+data_2024 <- read_excel("RawData/2024.xlsx")
+
+# Add YearOfSurvey column to 2023 and 2024
+data_2023 <- data_2023 %>%
+  mutate(YearOfSurvey = 2023) %>%
+  relocate(YearOfSurvey)
+
+data_2024 <- data_2024 %>%
+  mutate(YearOfSurvey = 2024) %>%
+  relocate(YearOfSurvey)
+
+
+
+
